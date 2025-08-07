292c72c9ee1b6b62c8e5b7731acd7d46e1699de5
bo <dodu7510@gmail.com>
2025-08-07 23:14:10 +0900

    feat: 상품 상세 조회 기능 추가

--- module-api/src/main/java/com/example/api/controller/product/ProductController.java
+++ module-api/src/main/java/com/example/api/controller/product/ProductController.java
@@ -1,6 +1,7 @@
 package com.example.api.controller.product;
 
 import com.example.api.dto.product.ProductResponse;
+import com.example.api.usecase.product.GetProductDetailUseCase;
 import com.example.api.usecase.product.ListProductsUseCase;
 import com.example.common.response.ApiResponse;
 import com.example.common.response.PageResponse;
@@ -10,6 +11,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
 	private final ListProductsUseCase listProductsUseCase;
+	private final GetProductDetailUseCase getProductDetailUseCase;
 
 	@GetMapping
 	public ResponseEntity<ApiResponse<PageResponse<ProductResponse>>> getAll(
@@ -26,6 +29,13 @@ public class ProductController {
 	) {
 		return ResponseEntity.status(HttpStatus.OK)
 			.body(ApiResponse.success(listProductsUseCase.getAll(pageable)));
+	}
+
+	@GetMapping("{productId}")
+	public ResponseEntity<ApiResponse<ProductResponse>> getProduct(
+		@PathVariable String productId) {
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(ApiResponse.success(getProductDetailUseCase.getProduct(productId)));
 	}
 
 }
--- module-api/src/main/java/com/example/api/facade/product/ProductFacade.java
+++ module-api/src/main/java/com/example/api/facade/product/ProductFacade.java
@@ -1,6 +1,8 @@
 package com.example.api.facade.product;
 
 import com.example.api.dto.product.ProductResponse;
+import com.example.common.exception.ApiException;
+import com.example.common.exception.ErrorType;
 import com.example.common.response.PageResponse;
 import com.example.domain.entity.Product;
 import com.example.domain.repository.ProductRepository;
@@ -9,6 +11,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -35,6 +38,21 @@ public class ProductFacade {
 
 		return PageResponse.of(list, pageable.getPageNumber(), page.getSize(),
 			page.getTotalElements());
+	}
+
+	public ProductResponse getProduct(String productId) {
+
+		Product product = productRepository.findById(Long.parseLong(productId))
+			.orElseThrow(() -> new ApiException("존재하지 않는 상품 번호 입니다.", ErrorType.INVALID_PARAMETER,
+				HttpStatus.BAD_REQUEST));
+
+		return new ProductResponse(
+			product.getId(),
+			product.getCategory().getName(),
+			product.getName(),
+			product.getDescription(),
+			product.getStock()
+		);
 	}
 
 }
