211e2d33dc412666e724ba3b2b26a3d802071c75
dodu232 <dodu7510@gmail.com>
2025-08-26 21:47:21 +0900

    feat: toss 결제 성공/실패 추가

--- module-api/src/main/java/com/example/api/controller/order/OrderWebController.java
+++ module-api/src/main/java/com/example/api/controller/order/OrderWebController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/api/v1/web/orders")
@@ -17,4 +18,20 @@ public class OrderWebController {
 		return "toss";
 	}
 
+	@GetMapping("/success")
+	public String successPage(
+		Model model,
+		@RequestParam String orderId,
+		@RequestParam String paymentKey,
+		@RequestParam int amount
+	) {
+		return "success";
+	}
+
+	@GetMapping("/fail")
+	public String failPage(
+
+	) {
+		return "fail";
+	}
 }
