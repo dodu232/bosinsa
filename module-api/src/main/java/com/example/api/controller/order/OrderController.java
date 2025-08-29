0501d46b8b2ab267a4bf48ba29881fd927121dbc
dodu232 <dodu7510@gmail.com>
2025-08-29 18:58:12 +0900

    feat: getOrder 메서드 추가

--- module-api/src/main/java/com/example/api/controller/order/OrderController.java
+++ module-api/src/main/java/com/example/api/controller/order/OrderController.java
@@ -2,14 +2,20 @@ package com.example.api.controller.order;
 
 import com.example.api.dto.order.OrderRequest;
 import com.example.api.dto.order.OrderResponse;
+import com.example.api.dto.order.PaymentComplete;
+import com.example.api.dto.order.PaymentInitResponse;
 import com.example.api.infra.auth.CustomUserDetails;
+import com.example.api.usecase.order.CompletePaymentUseCase;
 import com.example.api.usecase.order.CreateOrderUseCase;
+import com.example.api.usecase.order.GetOrderUseCase;
 import com.example.common.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +27,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
 	private final CreateOrderUseCase createOrderUseCase;
+	private final GetOrderUseCase getOrderUseCase;
+	private final CompletePaymentUseCase completePaymentUseCase;
+
 
 	@PostMapping
 	public ResponseEntity<ApiResponse<OrderResponse.Create>> createOrder(
@@ -28,6 +37,23 @@ public class OrderController {
 		@Valid @RequestBody OrderRequest.Create request) {
 		return ResponseEntity.status(HttpStatus.OK)
 			.body(ApiResponse.success(createOrderUseCase.createOrder(user.getId(), request)));
+	}
+
+	@GetMapping("/{orderId}")
+	public ResponseEntity<ApiResponse<PaymentInitResponse>> getOrder(
+		@PathVariable Long orderId
+	) {
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(ApiResponse.success(getOrderUseCase.getOrder(orderId)));
+	}
+
+	@PostMapping("/payment/complete")
+	public ResponseEntity<ApiResponse<String>> completePayment(
+		@RequestBody PaymentComplete request
+	) {
+		completePaymentUseCase.completePayment(request);
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(ApiResponse.success("결제 완료"));
 	}
 
 }
