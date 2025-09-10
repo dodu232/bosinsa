3fe449798aad53c1ef02ba50eafed26229e97397
dodu232 <dodu7510@gmail.com>
2025-09-10 15:07:36 +0900

    refactor: ProcessedEventRepository 제네릭 타입 수정

--- module-domain/src/main/java/com/example/domain/repository/ProcessedEventRepository.java
+++ module-domain/src/main/java/com/example/domain/repository/ProcessedEventRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProcessedEventRepository extends JpaRepository<ProcessedEvent, Long> {
+public interface ProcessedEventRepository extends JpaRepository<ProcessedEvent, String> {
 
 	@Modifying
 	@Query(value = "INSERT INTO processed_events(id) VALUES (:eventId)", nativeQuery = true)
