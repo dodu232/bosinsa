33b30f3d7eb165ead35b890646ed0d55ed9a0277
dodu232 <dodu7510@gmail.com>
2025-09-17 20:04:28 +0900

    feat: 주문 통계 집계 배치 기능 추가

--- module-domain/src/main/java/com/example/domain/support/BCryptPasswordEncryptor.java
+++ module-domain/src/main/java/com/example/domain/support/BCryptPasswordEncryptor.java
@@ -1,9 +1,11 @@
 package com.example.domain.support;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class BCryptPasswordEncryptor implements PasswordEncryptor {
 
 	private final BCryptPasswordEncoder delegate = new BCryptPasswordEncoder();
